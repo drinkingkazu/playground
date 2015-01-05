ddf1755cf0ea1075a3333538f2cccac51ae62eca
drinkingkazu <kazuhiro@nevis.columbia.edu>
2015-01-05 04:41:29 -0600

    Adding doxygen comments --kazu

--- ToyReco/ToyPulseRecoTemplate.h
+++ ToyReco/ToyPulseRecoTemplate.h
@@ -18,6 +18,10 @@
 #include "ToyPulse.h"
 /**
    \class ToyPulseRecoTemplate
+   @ Templated version of ToyPulseReco
+   See ToyPulseReco for details of how the algorithm works.\n
+   This class is a templated version of ToyPulseReco.\n
+   It can work with std::vector<T> where T can be anything that has an ordinary unary operator implemented.\n
  */
 template <class T>
 class ToyPulseRecoTemplate{
